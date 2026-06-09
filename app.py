e9114ce59cc3443821d98c2b770d1ff5fac95270
bayuzzs <bayufransdo@gmail.com>
2026-06-09 09:03:54 +0700

    feat: initialize dependencies, update ignore rules, and enable CORS in Flask app

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import joblib
 import numpy as np
 
 app = Flask(__name__)
+CORS(app)
 
 # Load model sekali saat server start
 model = joblib.load("slr_model.pkl")
